94061c521eacccaef9c840e78fb913e98fe2abd7
TAN SUAN LOONG <suanloongtan@gmail.com>
2025-10-22 02:06:34 +0000

    Ex2 completed

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,9 +3,22 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                input: "",
             }
-            
+        },
+        methods: {
+            del(i){
+                if (i > -1){
+                    this.items.splice(i, 1);
+                }
+            },
+
+            add(){
+                if (this.input != ""){
+                    this.items.push(this.input)
+                }
+            }
         }
     }
 </script>
@@ -13,6 +26,11 @@
 <template>
 
     <h2>Shopping Cart</h2>
+    <ul>
+        <li v-for="item,index in items" :key="index" :value="item">{{ item }} <button @click="del(index)">Delete</button></li>
+        
+    </ul>
     <!-- TODO: Add Code Here -->
+    <input type="text" v-model="input"><button @click="add">Add!</button>
    
 </template>
